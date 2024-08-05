a96043506863cd853bb7c583ca08a11caaebd987
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-08-05 17:08:53 +0200

    Update

--- src/random_walk.jl
+++ src/random_walk.jl
@@ -4,16 +4,14 @@ degree_matrix(A::SparseMatrixCSC) = Diagonal(length.(nzrange.(Ref(A), axes(A, 2)
 sumdropdims(x; dims) = dropdims(sum(x; dims); dims)
 
 function community_walk_probabilities(rng::AbstractRNG, sbm::SBM; nb_layers=1)
+    inds(c) = community_range(sbm, c)
     C = nb_communities(sbm)
     A = rand(rng, sbm)
     D = degree_matrix(A)
-    W = (D + I) \ (A + I)
+    W = inv(D + I) * (A + I)
     R = W^nb_layers
-    R2 = R .^ 2
-    r = [sumdropdims(R[:, community_range(sbm, c1)]; dims=2) for c1 in 1:C]
-    s = [sumdropdims(R2[:, community_range(sbm, c1)]; dims=2) for c1 in 1:C]
-    return (
-        [r[c1][community_range(sbm, c0)] for c0 in 1:C, c1 in 1:C],
-        [s[c1][community_range(sbm, c0)] for c0 in 1:C, c1 in 1:C],
-    )
+    R2 = abs2.(R)
+    s = [sumdropdims(R[:, inds(c1)]; dims=2)[inds(c0)] for c0 in 1:C, c1 in 1:C]
+    t = [sumdropdims(R2[:, inds(c1)]; dims=2)[inds(c0)] for c0 in 1:C, c1 in 1:C]
+    return s, t
 end
--- test/playground.jl
+++ test/playground.jl
@@ -6,10 +6,4 @@ rng = Random.default_rng()
 
 sbm = SBM(3000, 3, 0.02, 0.01)
 
-r, s = Oversmoothing.community_walk_probabilities(rng, sbm; nb_layers=2);
-
-fig = Figure()
-for c0 in 1:nb_communities(sbm), c1 in 1:nb_communities(sbm)
-    scatter!(Axis(fig[c0, c1]), r[c0, c1], s[c0, c1]; alpha=0.1)
-end
-fig
+s, t = Oversmoothing.community_walk_probabilities(rng, sbm; nb_layers=2);
